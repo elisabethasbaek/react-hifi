9054b5bba0a52c94442787daa2363465b222e7c8
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-05 09:30:46 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
+
+function App(){
+    return (
+        <>
+        
+        </>
+    );
+};
+
+render(<App/>, document.getElementById("root"));
