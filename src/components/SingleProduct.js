55ffddef8c17aaabebe9bcbbcd2846838b03f517
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-08 12:31:57 +0100

    api

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,10 +1,21 @@
-import React from "react";
-import "./SingleProduct.scss";
+import React, { useEffect, useState } from "react";
+import "./style/SingleProduct.scss";
 
 function SingleProduct({sku}){
-    return(
-        null
-    )
+
+    var [product, setProduct] = useState({});
+
+    useEffect(function(){
+        fetch("https://hifi-corner.herokuapp.com/api/v1/products/" + sku)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                setProduct(data);
+            });
+    }, []);
+
+    return <h1 className="singleProduct">{sku}</h1>
 };
 
 export default SingleProduct;
